727731421663957c650dd2631ea4b5b65594fe14
JUAN BERNAL MARTINEZ <juan.bernalm@alumno.buap.mx>
2023-01-26 23:43:45 -0600

    Agregar archivos de proyecto.

---
+++ Observer/Class1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer
+{
+        interface subject{
+            void registrarObservador();
+            void deleteObserver();
+
+            void notifyObserver();
+        }
+        interface observer
+        {
+            void update();
+        }
+
+    class tienda : subject
+    {
+        int i = 0;
+        private string[] reg = new string[10];
+        public void registrarObservador()
+        {
+            Console.WriteLine("Observador Registrado");
+        }
+        public void registrarObservador(cliente _cliente)
+        {
+            if (reg[i] == null)
+            {
+                reg[i] = _cliente.Dato;
+            }
+            else
+            {
+                i++;
+                reg[i] = _cliente.Dato;
+            }
+            Console.WriteLine("Observador Registrado");
+        }
+        public void deleteObserver() {
+            Console.WriteLine("Observador Eliminado");
+        }
+        public void deleteObserver(cliente _cliente)
+        {
+            for(int j = 0; j < reg.Length; j++)
+            {
+                if(reg[j] == _cliente.Dato)
+                {
+                    reg[j] = "";
+                }
+            }
+            Console.WriteLine("Observador " + _cliente.Dato + " Eliminado");
+        }
+
+        public void notifyObserver()
+        {
+            for(int j = 0; j<reg.Length; j++)
+            {
+                if(reg[j] != "" && reg[j] != null)
+                {
+                    Console.WriteLine("Notificacion Enviada a " + reg[j]);
+                }   
+            }
+        }
+    }
+
+    class cliente : observer
+    {
+        private string nombre;
+        public string Dato { get => nombre; set => nombre = value; }
+
+        public cliente(){
+
+        }
+        public cliente(string _nombre)
+        {
+            nombre = _nombre;
+        }
+    public void update()
+        {
+            Console.WriteLine("Actualizado");
+        }
+    }
+ 
+}
---
+++ Observer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            cliente[] obs = new cliente[10];
+
+            cliente Cliente1 = new cliente("Cliente1");
+            cliente Cliente2 = new cliente("Cliente2");
+            tienda tienda1 = new tienda();
+            tienda1.registrarObservador(Cliente1);
+            tienda1.registrarObservador(Cliente2);
+            tienda1.notifyObserver();
+            tienda1.deleteObserver(Cliente2);
+            Cliente1.update();
+            Cliente2.update();
+            tienda1.notifyObserver();
+
+            Console.ReadKey();
+        }
+    }
+}
